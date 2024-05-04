122170b110a62606d2995deae93f2234202e70f6
muhammadkashif20 <kashif28@gmail.com>
2024-05-04 00:29:45 -0700

    2 question pending

--- app.js
+++ app.js
@@ -52,12 +52,53 @@ console.log(-result);
 
 // Random Dise Value question 4
 
-var randomNumber=Math.random()*4
-var result=Math.round(randomNumber)
-console.log('Random dice value: '+result);
-var randomNumber2=Math.random()*6
-var result=Math.round(randomNumber2)
-console.log('Random dice value: '+result);
+// var randomNumber=Math.random()*4
+// var result=Math.round(randomNumber)
+// console.log('Random dice value: '+result);
+// var randomNumber2=Math.random()*6
+// var result=Math.round(randomNumber2)
+// console.log('Random dice value: '+result);
+
+// haids Tails Game question 5
+
+var game=Math.random()*2
+var result=Math.round(game)
+console.log(result);
+
+if(result==2){
+    console.log(result+' random coin value: Heads');
+}
+else if(result==1){
+    console.log(result+' random coin value: Tails');
+}
+else{
+    console.log('Try Again');
+}
+
+// Random Number Betwwen 1 to 100 question 6
+
+var generateNumber=Math.random()*100
+var result=Math.round(generateNumber)
+console.log('Random Number Between 1 and 100: '+result);
+
+// User weight question 7
+// alert(`a. 50b. 50kgsc. 50.2kgsd. 50.2kilograms`)
+// var userWeights=prompt('enter your weights in kilograms')
+// console.log('The Weight of user is: '+userWeights+' Kilograms');
+
+// Secret Number Game question 8
+
+var secretNumber=Math.random()*10
+var result=Math.round(secretNumber)
+console.log(result);
+var userNumber=prompt('enter your number ')
+if(result==userNumber){
+    alert('Congratulations The user');
+}
+else{
+    alert('Your number is wrong');
+
+}
 
 
 
